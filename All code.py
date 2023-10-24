272197c7b11eeeeedccb0c8dc87daa518e948e4a
mrsage-101 <abubakaryourkey309@gmail.com>
2023-10-25 03:02:48 +0500

    If conditions done

--- All code.py
+++ All code.py
@@ -180,3 +180,44 @@ elif age < 0:
     print("Your age is not valid")
 else:
     print("You're logged out")
+
+# another example
+
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y":
+    print("Have some food:!")
+elif response == "":
+    print("You didn't typed anything")
+else:
+    print("No food for you!")
+
+# dealing with boolean
+for_sale = True
+if for_sale:
+    print("This item is for_sale")
+else:
+    print("This item is not for sale") 
+
+# calculator program
+
+operator_choice = input("Enter operator (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator_choice == "+":
+    add = num1 + num2
+    print(f"Addtion result of {num1} + {num2}: {add}")
+elif operator_choice == "-":
+    sub = num1 - num2
+    print(f"Subtraction result of {num1} + {num2}: {sub}")
+elif operator_choice == "*":
+    mul = num1 * num2
+    print(f"Multiplication result of {num1} * {num2}: {mul}")
+elif operator_choice == "/":
+    div = num1 / num2
+    print(f"Division result of {num1} / {num2}: {div}")
+else:
+    print("You choose not the mentioned operator")
+
+
